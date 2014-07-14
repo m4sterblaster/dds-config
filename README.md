e1efbccac64b4deb8481812dc68c91b07e3b2bf5
M4sterBlaster <m4sterblaster@gmail.com>
2014-07-14 12:11:21 +0100

    make install file for ubuntu and centos

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
-dds-config
-==========
+#devenv-config
 
-<ul>Configures local dds
-<li>sudo ./bin/dev run </li>
-<li>./bootstrap </li>
-<ul>
+## Overview
+Config for *nix dev env. 
+* download utils: curl, wget, tree, vim, exuberant-ctags (a dependency for a vim plugin), git, tmux
+* set vim as default editor, add plugins (see .vim) 
+* configure git with your username and email
 
-<p>
+
+## Installation instructions
+* ./ubuntu_install for Ubuntu
+* ./centos_install for Centos
+
+
 Note: To add any new vim plugins, just unzip plugins at .vim directory.
-</p>
---
+++ ubuntu_install.sh
@@ -0,0 +1,22 @@
+# install common utils
+sudo apt-get install curl wget tree vim exuberant-ctags tmux
+sudo apt-get install tmux
+
+# set vim as default editor
+sudo ln -sf /usr/bin/vim.basic /etc/alternatives/editor 
+
+# configure git globally
+echo "What is your github user name?"
+read github_username
+git config --global user.name $github_username
+echo "What is your github email?"
+read github_email
+git config --global user.email $github_email
+echo "git is now configured globally, don't forget to add key to github"
+
+# configure vim and stuff
+cp -rf .vim ~/
+cp .vimrc ~/
+cp .rdebugrc ~/
+cp .tmux.conf ~/
+mkdir ~/.NERDTreeBookmarks
